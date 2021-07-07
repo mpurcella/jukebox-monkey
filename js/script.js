1470d3b9934eed2d0b065b5cc807f0686a2eef3a
Michael Purcella <mjpurcella@gmail.com>
2021-07-07 14:31:59 -0700

    updated modal code

--- js/script.js
+++ js/script.js
@@ -32,22 +32,11 @@ $(document).ready(function () {
 	});
 
 	// Modal Video Player - BMTH
-
-	let $videoSrc;
-	$('.card').click(function () {
-		$videoSrc = $(this).attr('data-tagVideo');
-	});
-	console.log($videoSrc);
-
-	// when the modal is opened autoplay it
-	$('#videoModalBMTH').on('shown.bs.modal', function (e) {
-		// set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
-		$('#video').attr('src', $videoSrc + '?autoplay=1&amp;modestbranding=1&amp;showinfo=0');
+	$('#vidBMTH').click(function () {
+		$('#modalVideoBMTH').modal('show');
 	});
 
-	// stop playing the youtube video when I close the modal
-	$('#videoModalBMTH').on('hide.bs.modal', function (e) {
-		// a poor man's stop video
-		$('#video').attr('src', $videoSrc);
+	$('#modalVideoBMTH').on('hide.bs.modal', function () {
+		$('#vidBMTHFrame').attr('src', $('#video101').attr('src'));
 	});
 });
